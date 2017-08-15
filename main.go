c6c9933091a8315eac08022671fba7cbab3976dd
Rick Emory <rckemry@gmail.com>
2017-08-15 10:28:13 -0400

    added log back

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+    log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), nil))
 
 }
